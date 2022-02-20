4daf27d6ad099834e72b0ff0efdd4e72614c6bfd
Stepan <stepandokukin@gmail.com>
2022-02-20 12:14:19 +0300

    made a comics list page

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import './randomChar.scss';
@@ -7,41 +7,27 @@ import mjolnir from '../../resources/img/mjolnir.png';
 
 const RandomChar = (props) =>  {
     const [char, setChar] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
 
-    const marvelService = new MarvelService();
+    const {loading, error, getCharacter, clearError} = useMarvelService();
 
     useEffect(() => {
         updateChar();
-        const timerId = setInterval(updateChar, 3000);
+        const timerId = setInterval(updateChar, 600000);
 
         return () => {
             clearInterval(timerId);
         }
     }, [])
-    
-    const onCharLoading = () => {
-        setLoading(true);
-    }
 
     const onCharLoaded = (char) => {
-        setLoading(false);
         setChar(char);
     }
 
-    const onError = () => {
-        setLoading(false);
-        setError(true);
-    }
-
     const updateChar = () => {
+        clearError();
         const id = Math.floor(Math.random() * (1011400-1011000) + 1011000);
-        onCharLoading();
-        marvelService
-            .getCharacter(id)
-            .then(onCharLoaded)
-            .catch(onError)
+        getCharacter(id)
+            .then(onCharLoaded);
     }
 
     const errorMessage = error ? <ErrorMessage/> : null;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,32 +1,36 @@
+import {useHttp} from '../hooks/http.hook';
 
+const useMarvelService = () => {
 
-class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=c594be2b12b4d62e4994f1e4f7121048';
-    _baseOffset = 210;
+    const {loading, request, error, clearError } = useHttp();
+
+    const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    const _apiKey = 'apikey=c594be2b12b4d62e4994f1e4f7121048';
+    const _baseOffset = 210;
     // offsetRandom = Math.floor(Math.random() * (1500));
 
-    getResource = async (url) => {
-        let res = await fetch(url);
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-
-        return await res.json();
+    const getAllCharacters = async (offset = _baseOffset) => {
+        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_tranformCharacter);
     }
 
-    getAllCharacters = async (offset = this._baseOffset) => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
-        return res.data.results.map(this._tranformCharacter);
+    const getCharacter = async (id) => {
+        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+        return _tranformCharacter(res.data.results[0]);
     }
 
-    getCharacter = async (id) => {
-        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._tranformCharacter(res.data.results[0]);
+    const getAllComics = async (offset = 0) => {
+        const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=$${offset}&${_apiKey}`);
+        return res.data.results.map(_tranformComics);
     }
 
-    _tranformCharacter = (char) => {
+    const getComics = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _tranformComics(res.data.results[0]);
+    }
+
+    const _tranformCharacter = (char) => {
         return {
             id: char.id,
             name: char.name,
@@ -37,6 +41,20 @@ class MarvelService {
             comics: char.comics.items 
         }
     }
+
+    const _tranformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            description: comics.description || 'There is no description',
+            pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
+            thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+            language: comics.textObjects.language || 'en-us',
+            price: comics.prices.price ? `${comics.prices.price}$` : 'not available'
+        }
+    }
+
+    return {loading, error, getAllCharacters, getCharacter,getAllComics, getComics, clearError}
 }
 
-export default MarvelService;
+export default useMarvelService;
