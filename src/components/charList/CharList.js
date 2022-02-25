1fc09e41899e5fed0135677b15ca5fa26bfacdbd
Stepan <stepandokukin@gmail.com>
2022-02-25 12:18:24 +0300

    added transition to char-list

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const CharList = (props) => {
 
@@ -53,29 +54,35 @@ const CharList = (props) => {
             }   
             
             return (
-                <li className='char__item' 
+                <CSSTransition
                     key={item.id}
-                    tabIndex={0}
-                    ref={(el) => itemRefs.current[i] = el }
-                    onClick={() => {
-                        props.onCharSelected(item.id);
-                        focusOnItem(i);
-                    }}
-                    onKeyPress={(e) => {
-                        if (e.key === ' ' || e.key === "Enter") {
+                    timeout={500}
+                    classNames="char__item">
+                    <li className='char__item' 
+                        tabIndex={0}
+                        ref={(el) => itemRefs.current[i] = el }
+                        onClick={() => {
                             props.onCharSelected(item.id);
-                        focusOnItem(i);
-                        }
-                    }}>
-                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-                        <div className="char__name">{item.name}</div>
-                </li>
+                            focusOnItem(i);
+                        }}
+                        onKeyPress={(e) => {
+                            if (e.key === ' ' || e.key === "Enter") {
+                                props.onCharSelected(item.id);
+                            focusOnItem(i);
+                            }
+                        }}>
+                            <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                            <div className="char__name">{item.name}</div>
+                    </li>
+                </CSSTransition>
             )
         });
 
         return (
             <ul className="char__grid">
-                {items}
+                <TransitionGroup component={null}>
+                    {items}
+                </TransitionGroup>
             </ul>
         )
     }
